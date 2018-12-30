fe1624a193cf8c3d203baa723a8a4b54e7db9c44
Nikohtr <Niko.htr@gmail.com>
2018-12-30 18:41:33 +0200

    Update main.py

--- main.py
+++ main.py
@@ -32,13 +32,13 @@ async def esay(ctx, *, mg = None):
         await client.send_message(ctx.message.channel, embed = discord.Embed(description = "["+mg+"](https://www.youtube.com/user/PewDiePie?sub_confirmation=1)", color = 0x2b44ff))
 
 @client.command(pass_context = True)
-async def link(ctx, *, mg = None, link = None):
+async def link(ctx, mg = None, link = None):
     if ctx.message.author.id == "263685060819943425":
       await client.delete_message(ctx.message)
 
       if not mg: await client.say("Please specify a message to send")
       else:
-        await client.send_message(ctx.message.channel, embed = discord.Embed(description = "["+mg+"]("+link+")", color = 0x2b44ff))
+        await client.send_message(ctx.message.channel, embed = discord.Embed(description = "["+str(mg)+"]("+str(link)+")", color = 0x2b44ff))
 
         
 @client.command()
