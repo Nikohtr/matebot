38caf3d36ece3dce3016f5655047bf747f2834cb
Nikohtr <Niko.htr@gmail.com>
2019-01-14 23:26:58 +0200

    Update main.py

--- main.py
+++ main.py
@@ -135,11 +135,12 @@ async def sub():
         subst = json.loads(data1)["items"][0]["statistics"]["subscriberCount"]
         data = urllib.request.urlopen("https://www.googleapis.com/youtube/v3/channels?part=statistics&forUsername=pewdiepie&key="+key).read()
         subspew = json.loads(data)["items"][0]["statistics"]["subscriberCount"]
-        subsnow = int(subspew)/100000
+        subsnow = int(subspew)//100000
         async for message in client.logs_from(client.get_channel("532571319196188712"), limit=1):
                 if message.author == client.user:
                     subsbefore = int(message.content)
         if subsnow!=subsbefore:
+            print(subspew)
             await client.send_message(client.get_channel("532571319196188712"), subsnow)
             await client.send_message(client.get_channel("528874952342896640"), "@everyone PewDiePie just hit {:,d}".format(subsnow*100000))
         if int(subst)>int(subspew):
