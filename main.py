bd9fce162554cf60b95fc49944c4399379254773
Nikohtr <Niko.htr@gmail.com>
2019-01-25 18:10:48 +0200

    Update main.py

--- main.py
+++ main.py
@@ -242,7 +242,7 @@ async def on_member_join(member):
 @client.event
 async def on_message_delete(message):
     if message.author!=client.user:
-        await client.send_message(client.get_channel("517753229258391567"), embed = discord.Embed(description ="Message sent by "+str(message.author)+" was deleted in "+str(message.channel)+"\n\n"+message.content, color = 0x2b44ff))
+        await client.send_message(client.get_channel("517753229258391567"), embed = discord.Embed(description ="Message sent by **"+str(message.author)+"** was deleted in **"+str(message.channel)+"**\n\n**"+message.content+"**", color = 0x2b44ff))
     
 
 @client.event
@@ -256,9 +256,9 @@ async def change_playing():
   
 @client.event
 async def on_message_edit(old, new):
-    if message.author!=client.user:
+    if new.author!=client.user:
         await on_message(new)
-        await client.send_message(client.get_channel("517753229258391567"), embed = discord.Embed(description ="Message sent by "+str(new.author)+" in "+str(new.channel)+" was edited\n\nOld \n"+str(old.content)+"\n\nNew \n"+str(new.content), color = 0x2b44ff))
+        await client.send_message(client.get_channel("517753229258391567"), embed = discord.Embed(description ="Message sent by **"+str(new.author)+"** in **"+str(new.channel)+"** was edited\n\nOld \n**"+str(old.content)+"**\n\nNew \n**"+str(new.content)+"**", color = 0x2b44ff))
 
 
 @client.event
